40a22ac39f774841b917fdbd2ac92913b3f7a2c9
yaiza22 <yaizas719@gmail.com>
2026-05-23 19:25:30 -0500

    Merge con develop, develop se actualizó después de que esta rama se hubiera creado

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
